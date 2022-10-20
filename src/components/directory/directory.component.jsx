757d3adcf76485d06f880fad2c98d621fd001d61
tylern06 <tynguyen06@gmail.com>
2022-10-20 16:26:05 -0700

    add firebase and navbar component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import CategoryItem from '../category-item/category-item.component';
+import './directory.styles.scss';
 
 export default function Directory({ categories }) {
   return (
-    <div className="categories-container">
+    <div className="directory-container">
       {categories.map((category) => (
         <CategoryItem key={category.id} category={category} />
       ))}
