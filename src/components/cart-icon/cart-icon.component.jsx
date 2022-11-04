10ace2c5b4699c413b67363c62a07dd354a06917
tylern06 <tynguyen06@gmail.com>
2022-11-04 15:52:32 -0700

    add useMemo on item count value to rerender when cartItems changes

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useCallback } from 'react';
+import React, { useContext, useMemo } from 'react';
 import './cart-icon.styles.scss';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import { CartContext } from '../../contexts/cart.context';
@@ -11,16 +11,24 @@ function CardIcon() {
     setIsCartOpen(!isCartOpen);
   };
 
-  const itemCount = useCallback(() => {
-    return cartItems.reduce((total, currentItem) => {
-      return total + currentItem.quantity;
-    }, 0);
-  }, [cartItems]);
+  // memoize quantity count to prevent recompute when cart dropdown is open
+  const itemCount = useMemo(
+    () =>
+      cartItems.reduce((total, currentItem) => {
+        console.log('compute');
+        return total + currentItem.quantity;
+      }, 0),
+    [cartItems]
+  );
 
+  // const itemCount = cartItems.reduce((total, currentItem) => {
+  //   console.log('compute', total);
+  //   return total + currentItem.quantity;
+  // }, 0);
   return (
     <div className="cart-icon-container" onClick={toggleIsCartOpen}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">{itemCount()}</span>
+      <span className="item-count">{itemCount}</span>
     </div>
   );
 }
