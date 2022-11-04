7e4a2447e86e392df53804f519af9664ffb5d980
tylern06 <tynguyen06@gmail.com>
2022-11-04 13:26:42 -0700

    display cart time total quantity

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,21 +1,26 @@
-import React, { useContext } from 'react';
+import React, { useContext, useCallback } from 'react';
 import './cart-icon.styles.scss';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import { CartContext } from '../../contexts/cart.context';
 
 function CardIcon() {
-  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const { cartItems, isCartOpen, setIsCartOpen } =
+    useContext(CartContext);
 
   const toggleIsCartOpen = () => {
     setIsCartOpen(!isCartOpen);
   };
+
+  const itemCount = useCallback(() => {
+    return cartItems.reduce((total, currentItem) => {
+      return total + currentItem.quantity;
+    }, 0);
+  }, [cartItems]);
+
   return (
-    <div className="cart-icon-container">
-      <ShoppingIcon
-        className="shopping-icon"
-        onClick={toggleIsCartOpen}
-      />
-      <span className="item-count">0</span>
+    <div className="cart-icon-container" onClick={toggleIsCartOpen}>
+      <ShoppingIcon className="shopping-icon" />
+      <span className="item-count">{itemCount()}</span>
     </div>
   );
 }
